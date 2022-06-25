8c66e4e0591bf74b23b2ac9f82e783f159aa3022
bobbyrinaldo29 <bobbyrinaldo@icloud.com>
2022-06-25 08:54:47 +0700

    fix navbar menu

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
         {Object.entries(request).map(([key, { title, url }]) => {
           return (
             <Link href={`/?genre=${key}`} key={key}>
-              <a className="transition duration-100 transform cursor-pointer hover:scale-125 hover:text-white active:text-red-500">
+              <a className="transition duration-100 transform cursor-pointer hover:scale-125 last:pr-24 first:pl-24 hover:text-white active:text-red-500">
                 {title}
               </a>
             </Link>
